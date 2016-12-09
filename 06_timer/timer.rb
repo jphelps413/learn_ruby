88df404ff69bb856f44c8567ece3d66a146e263d
John Phelps <john@fractograf.com>
2016-12-09 11:16:19 -0500

    Should simply use string formatting here

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  # Pretty sure the intention is implement logic to pad as opposed to using
+  # the built in string formatting.
+  def padded t
+    # This would be cleaner:
+    #     "%02d" % [t]
+    # but I don't think that's the intention of the exercise?
+    (t < 10 ? "0"+t.to_s : t).to_s
+  end
+
+  # This is supposed to be an exercise in string manipulation, but I am
+  # sorely tempted to use the Time class.
+  def time_string
+    # This would be cleaner:
+    #    "%02d:%02d:%02d" % [@seconds/(60*60), (@seconds/60)%60, @seconds%60]
+    # but I don't think that's the intention of the exercise?
+    padded( @seconds/(60*60)) + ":" +
+    padded((@seconds/60)%60)  + ":" +
+    padded( @seconds%60)
+  end
 end
