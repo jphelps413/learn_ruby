23f143776ab6558bfe957936785f475e121ae19b
John Phelps <john@fractograf.com>
2016-12-09 15:50:43 -0500

    Cleaned up the simon repeater

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -2,7 +2,7 @@
 # -*- mode: ruby -*
 # vi: set ft=ruby :
 
-# Note the utter lack of inut checking, which is not recommended in a
+# Note the utter lack of input checking, which is not recommended in a
 # production environment, but is ok here because I said so.
 
 def echo e
@@ -13,12 +13,8 @@ def shout s
   s.upcase
 end
 
-# Initializing count to 2 lets us leave off that parameter and
-# still have the word word repeated.
 def repeat w,n=2
-  result = w
-  (n-1).times { result += " "+w }
-  result
+  (("#{w} ")*n).chomp(' ')
 end
 
 def start_of_word w,c
